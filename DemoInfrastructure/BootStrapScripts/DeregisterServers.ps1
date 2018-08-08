84e9eec463a10ed0f700b763a2561ea034ee505b
Bob Walker <bob.walker@octopus.com>
2018-08-08 15:36:39 -0500

    Adding the server name to the deregister message

--- DemoInfrastructure/BootStrapScripts/DeregisterServers.ps1
+++ DemoInfrastructure/BootStrapScripts/DeregisterServers.ps1
@@ -1,5 +1,6 @@
 $octopusServerUrl = $OctopusParameters["Global.Octopus.Url"]
 $octopusApiKey = $OctopusParameters["Global.Octopus.ApiKey"]
+$serverName = "#{Project.AWS.InstanceName}"
 Set-Location "${env:ProgramFiles}\Octopus Deploy\Tentacle"
 
 & .\tentacle.exe deregister-from --server $octopusServerUrl --apiKey $octopusApiKey --multiple --console | Write-Output
@@ -7,6 +8,6 @@ Set-Location "${env:ProgramFiles}\Octopus Deploy\Tentacle"
  $slackBody = @{
 	"channel" = "#demo-env-pulse"
 	"username" = "Register Additional Servers"
-	"text" = ":wave: Successfully deregistered the server from $octopusServerUrl"
+	"text" = ":wave: Successfully deregistered the server $serverName from $octopusServerUrl"
 }
 Invoke-WebRequest -Method POST -Uri https://hooks.slack.com/services/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 -Body (ConvertTo-Json -Compress -InputObject $slackBody) -UseBasicParsing
