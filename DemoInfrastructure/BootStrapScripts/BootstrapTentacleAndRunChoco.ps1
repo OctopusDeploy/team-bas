6d356cdca57037eb0897f3decee449ca742d436a
Bob Walker <bob.walker@octopus.com>
2018-11-19 14:51:29 -0600

    Fixing the chocolatey goodness

--- DemoInfrastructure/BootStrapScripts/BootstrapTentacleAndRunChoco.ps1
+++ DemoInfrastructure/BootStrapScripts/BootstrapTentacleAndRunChoco.ps1
@@ -12,30 +12,18 @@ Write-Output "InstanceName: $instanceName"
 Write-Output "SlackNotificationUrl: $slackNotificationUrl"
 Write-Output "ChocolateyAppList: $chocolateyAppList"
 
-function Get-FileFromServer 
-{ 
-	param ( 
-	  [string]$url, 
-	  [string]$saveAs 
-	) 
-
-	Write-Host "Downloading $url to $saveAs" 
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
-	$downloader = new-object System.Net.WebClient 
-	$downloader.DownloadFile($url, $saveAs) 
-} 
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+& choco install octopusdeploy.tentacle /y
 
 if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){
-	Write-Host "Chocolatey Apps Specified, installing chocolatey and applications"
-	
-	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	Write-Host "Chocolatey Apps Specified, installing chocolatey and applications"	
 	
 	$appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
 
 	foreach ($app in $appsToInstall)
 	{
 		Write-Host "Installing $app"
-		choco install $app /y
+		& choco install $app /y 
 	}
 }
 
@@ -46,27 +34,9 @@ if ($OctoTentacleService -eq $null)
     $tentacleListenPort = 10933 
     $tentacleHomeDirectory = "C:\Octopus" 
     $tentacleAppDirectory = "C:\Octopus\Applications" 
-    $tentacleConfigFile = "C:\Octopus\Tentacle\Tentacle.config"  
-    $tentacleDownloadPath = "https://octopus.com/downloads/latest/WindowsX64/OctopusTentacle" 	
-	
-	$tentaclePath = "C:\Tools\Octopus.Tentacle.msi" 
+    $tentacleConfigFile = "C:\Octopus\Tentacle\Tentacle.config"      	
 
-    Write-Output "Beginning Tentacle installation"     
-
-	Write-Output "Downloading latest Octopus Tentacle MSI..." 
-
-	$tentaclePath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(".\Tentacle.msi") 
-	if ((test-path $tentaclePath) -ne $true) { 
-	  Get-FileFromServer $tentacleDownloadPath $tentaclePath 
-	} 
-
-	Write-Output "Installing MSI" 
-	$msiExitCode = (Start-Process -FilePath "msiexec.exe" -ArgumentList "/i Tentacle.msi /quiet" -Wait -Passthru).ExitCode 
-	Write-Output "Tentacle MSI installer returned exit code $msiExitCode" 
-	if ($msiExitCode -ne 0) { 
-	  throw "Installation aborted" 
-	} 	
-
+    Write-Output "Beginning Tentacle installation"     	
     
     Write-Output "Open port $tentacleListenPort on Windows Firewall" 
     & netsh.exe firewall add portopening TCP $tentacleListenPort "Octopus Tentacle" 
@@ -74,7 +44,6 @@ if ($OctoTentacleService -eq $null)
         throw "Installation failed when modifying firewall rules" 
     } 
 
-    Write-Host "Getting public IP address"     
     Write-Output "Configuring and registering Tentacle" 
 
     Set-Location "${env:ProgramFiles}\Octopus Deploy\Tentacle" 	
@@ -82,53 +51,53 @@ if ($OctoTentacleService -eq $null)
 	$slackBody = @{
 		"channel" = "#demo-env-pulse"
 		"username" = "Cloud Formation PowerShell Bootstrap"
-		"text" = ":woohoo: Installation of bootstrap tentacle on $registerComputerName was successful"
+		"text" = ":woohoo: Installation of bootstrap tentacle on $instanceName was successful"
 	}
 
 	& .\tentacle.exe create-instance --instance "Tentacle" --config $tentacleConfigFile --console | Write-Output
 	if ($lastExitCode -ne 0) { 
-	 $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on create-instance for $registerComputerName."
+	 $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on create-instance for $instanceName."
 	 Invoke-WebRequest -Method POST -Uri $slackNotificationUrl -Body (ConvertTo-Json -Compress -InputObject $slackBody) -UseBasicParsing
 	 throw "Installation failed on create-instance" 
 	} 
 	& .\tentacle.exe configure --instance "Tentacle" --home $tentacleHomeDirectory --console | Write-Output
 	if ($lastExitCode -ne 0) { 
-	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on configure home directory for $registerComputerName."
+	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on configure home directory for $instanceName."
 	  Invoke-WebRequest -Method POST -Uri $slackNotificationUrl -Body (ConvertTo-Json -Compress -InputObject $slackBody) -UseBasicParsing
 	  throw "Installation failed on configure" 
 	} 
 	& .\tentacle.exe configure --instance "Tentacle" --app $tentacleAppDirectory --console | Write-Output
 	if ($lastExitCode -ne 0) { 
-	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on configure app directory for $registerComputerName."
+	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on configure app directory for $instanceName."
 	  Invoke-WebRequest -Method POST -Uri $slackNotificationUrl -Body (ConvertTo-Json -Compress -InputObject $slackBody) -UseBasicParsing
 	  throw "Installation failed on configure" 
 	} 
 	& .\tentacle.exe configure --instance "Tentacle" --port $tentacleListenPort --console | Write-Output
 	if ($lastExitCode -ne 0) { 
-	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on configure listen port for $registerComputerName."
+	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on configure listen port for $instanceName."
 	  Invoke-WebRequest -Method POST -Uri $slackNotificationUrl -Body (ConvertTo-Json -Compress -InputObject $slackBody) -UseBasicParsing
 	  throw "Installation failed on configure" 
 	} 
 	& .\tentacle.exe new-certificate --instance "Tentacle" --console | Write-Output
 	if ($lastExitCode -ne 0) { 
-	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on creating new certificate for $registerComputerName."
+	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on creating new certificate for $instanceName."
 	  Invoke-WebRequest -Method POST -Uri $slackNotificationUrl -Body (ConvertTo-Json -Compress -InputObject $slackBody) -UseBasicParsing
 	  throw "Installation failed on creating new certificate" 
 	} 
 	& .\tentacle.exe configure --instance "Tentacle" --trust $octopusServerThumbprint --console | Write-Output
 	if ($lastExitCode -ne 0) { 
-	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on configuring octopus server thumbprint for $registerComputerName."
+	  $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on configuring octopus server thumbprint for $instanceName."
 	  Invoke-WebRequest -Method POST -Uri $slackNotificationUrl -Body (ConvertTo-Json -Compress -InputObject $slackBody) -UseBasicParsing
 	  throw "Installation failed on configure" 
 	} 	                  	
 	& .\tentacle.exe service --instance "Tentacle" --install --start --console | Write-Output
 	if ($lastExitCode -ne 0) { 
-	   $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on install for $registerComputerName."
+	   $slackBody["text"] = ":sadpanda: \<!channel\> Installation failed on install for $instanceName."
 	   Invoke-WebRequest -Method POST -Uri $slackNotificationUrl -Body (ConvertTo-Json -Compress -InputObject $slackBody) -UseBasicParsing
 	  throw "Installation failed on service install" 
 	} 
 	
-	$slackBody["text"] = ":woohoo: Installation of bootstrap tentacle on $registerComputerName was successful."
+	$slackBody["text"] = ":woohoo: Installation of bootstrap tentacle on $instanceName was successful."
 	Invoke-WebRequest -Method POST -Uri $slackNotificationUrl -Body (ConvertTo-Json -Compress -InputObject $slackBody) -UseBasicParsing
 	Write-Output "Tentacle commands complete"     
 } else {
