0152b1240e0ebad25b65977dd1195ec7df75fb12
Bob Walker <bob.walker@octopus.com>
2019-01-25 09:57:15 -0600

    Removing/updating bootstrap scripts

--- DemoInfrastructure/BootStrapScripts/ASPNetIIS.ps1
+++ DemoInfrastructure/BootStrapScripts/ASPNetIIS.ps1
@@ -8,7 +8,3 @@ Dism /Online /Enable-Feature /FeatureName:IIS-CertProvider /All | Write-Output
 
 Write-Output "Installing IIS Management"
 Dism /Online /Enable-Feature /FeatureName:IIS-ManagementService /All | Write-Output
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-choco install dotnet4.7.1 -y
