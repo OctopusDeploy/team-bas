69494124f92209753d2ea62d5413975b9bdeb757
Bob Walker <bob.walker@octopus.com>
2019-02-14 14:14:17 -0600

    Making the script handle multiple runs

--- DemoInfrastructure/BootStrapScripts/SqlAdminLogin.ps1
+++ DemoInfrastructure/BootStrapScripts/SqlAdminLogin.ps1
@@ -12,36 +12,52 @@ function Get-FileFromServer
 	$downloader = new-object System.Net.WebClient 
 	$downloader.DownloadFile($url, $saveAs) 
 } 
-
-$ErrorActionPreference = 'Stop';
- 
-$packageName= 'sql-server-express'
-$url        = ''
-$url64      = 'https://download.microsoft.com/download/E/F/2/EF23C21D-7860-4F05-88CE-39AA114B014B/SQLEXPR_x64_ENU.exe'
-$checksum   = '71321559eff923066799d7fc26b6c7aedd41eee1'
-$silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE"
- 
-$tempDir = "C:\SQLServerExpress"
- 
-if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-$fileFullPath = "$tempDir\SQLEXPR.exe"
-
-Get-FileFromServer $url64 $fileFullPath
- 
-Write-Host "Extracting..."
-$extractPath = "$tempDir\SQLServerExpresInstall"
-Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
- 
-Write-Host "Installing..."
-$setupPath = "$extractPath\setup.exe"
-
-Start-Process $setupPath "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE" -Wait
-
 $octopusAdminDatabaseUser = "#{Global.Database.AdminUser}"
 $octopusAdminDatabasePassword = "#{Global.Database.AdminPassword}"
 $octopusAdminDatabaseServer = "#{Global.Database.Server}"
 
 $connectionString = "Server=$octopusAdminDatabaseServer;Database=master;integrated security=true;"
+
+$ErrorActionPreference = 'Stop';
+
+$InstallSQLServer = $true
+
+try
+{
+	$testsqlConnection = New-Object System.Data.SqlClient.SqlConnection
+	$testsqlConnection.ConnectionString = $connectionString
+
+	$testcommand = $testsqlConnection.CreateCommand()
+	$testcommand.CommandType = [System.Data.CommandType]'Text'
+
+	Write-Output "Opening the connection to $octopusAdminDatabaseServer"
+	$testsqlConnection.Open()
+	$InstallSQLServer = $false
+}
+catch
+{
+	$InstallSQLServer = $true
+}
+
+if ($InstallSQLServer -eq $true)
+{
+	$url64      = 'https://download.microsoft.com/download/E/F/2/EF23C21D-7860-4F05-88CE-39AA114B014B/SQLEXPR_x64_ENU.exe'
+	$tempDir = "C:\SQLServerExpress"
+	 
+	if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
+	$fileFullPath = "$tempDir\SQLEXPR.exe"
+
+	Get-FileFromServer $url64 $fileFullPath
+	 
+	Write-Host "Extracting..."
+	$extractPath = "$tempDir\SQLServerExpresInstall"
+	Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
+	 
+	Write-Host "Installing..."
+	$setupPath = "$extractPath\setup.exe"
+
+	Start-Process $setupPath "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE" -Wait
+}
 
 $sqlConnection = New-Object System.Data.SqlClient.SqlConnection
 $sqlConnection.ConnectionString = $connectionString
@@ -52,9 +68,14 @@ $command.CommandType = [System.Data.CommandType]'Text'
 Write-Output "Opening the connection to $octopusAdminDatabaseServer"
 $sqlConnection.Open()
 
-Write-Output "Running the if not exists then create user command on the server"
-$command.CommandText = "CREATE LOGIN [$octopusAdminDatabaseUser] with Password='$octopusAdminDatabasePassword', default_database=master"            
-$command.ExecuteNonQuery()
+try{
+	Write-Output "Running the if not exists then create user command on the server"
+	$command.CommandText = "CREATE LOGIN [$octopusAdminDatabaseUser] with Password='$octopusAdminDatabasePassword', default_database=master"            
+	$command.ExecuteNonQuery()
+}
+catch{
+	Write-Host "The user already exists, no need to add it"
+}
 
 Write-Output "Granting the sysadmin role to $octopusAdminDatabaseUser"
 $command.CommandText = "sp_addsrvrolemember @loginame= '$octopusAdminDatabaseUser', @rolename = 'sysadmin'"  
